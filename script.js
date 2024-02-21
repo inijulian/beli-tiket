52f087128dc2e543629879d3663e5d217ad3f111
inijulian <julianraditya82@gmail.com>
2024-02-21 14:03:38 +0700

    add file index.html dan script.js

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ var umur = prompt("Masukkan Umur Anda");
 
 if (umur >= 13) {
     const studio = prompt("Pilih studio A/B/C");
-    alert(`Pembelian Tiket Berhasil, Detail Tiket: \n\n
-           Nama ${nama} \n
-           Umur ${umur} \n
-           Studio ${studio}`);
+    alert(`Pembelian Tiket Berhasil, Detail Tiket: \n\nNama ${nama} \nUmur ${umur} \nStudio ${studio}`);
 } else {
     alert("Maaf, Umur Anda Belum 13");
 }
